ecb935717d108f31e256dc5d41e419e073930587
Harshit Rai <raiharshit66@gmail.com>
2024-12-22 23:03:33 +0530

    Add the declaration  files to typescript and npmigore file for src

---
+++ common/dist/index.d.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+export declare const signedInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}>;
+export type SignupInfer = z.infer<typeof signedInput>;
