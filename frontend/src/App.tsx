c96713584103c5d6c62234119a2f2e2f67b863be
Harshit Rai <raiharshit66@gmail.com>
2024-12-23 17:08:37 +0530

    Add the Reacter - Router -Dom to handle frontend page routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import "./App.css"
-
+import { BrowserRouter , Routes, Route} from 'react-router-dom'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+import { Blog } from './pages/Blog'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-   <></>
+   <>
+   <BrowserRouter>
+   <Routes>
+<Route path='/signin' element = {<Signin/>}></Route>
+<Route path='/signup' element = {<Signup/>}></Route>
+<Route path='/blog/:id' element = {<Blog/>}></Route>
+{/* <Route path='/signin' element = {<Signin/>}></Route> */}
+   </Routes>
+   </BrowserRouter></>
   )
 }
 
