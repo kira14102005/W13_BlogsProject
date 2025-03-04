bbeae7037764edfeb88d6899be6c004563da78ab
Harshit Rai <raiharshit66@gmail.com>
2025-03-04 22:52:07 +0530

    Create a blogrouter that could handle the createBlog post requests to the server and uses the AuthMidware and Zod Validation for the same

---
+++ backend/src/router/blogRouter.ts
@@ -0,0 +1,43 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { authMiddleware } from "../middlewares/auth";
+import { validateCreateBlog, validateUpdateBlog } from "../middlewares/validation";
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    JWT_KEY: string;
+    DATABASE_URL: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+
+blogRouter.use("/*", authMiddleware);
+
+blogRouter.post("/create", validateCreateBlog, async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+      }).$extends(withAccelerate());
+      
+  const body = await c.req.json();
+  const authorID = c.get("userId");
+
+  try {
+    const blog = await prisma.post.create({
+      data: {
+        authorId: authorID,
+        title: body.title,
+        desc: body.desc,
+      },
+    });
+
+    return c.json({ msg: "Blog Created successfully", id: blog.id });
+  } catch (e: any) {
+    c.status(500);
+    return c.json({ msg: "Error in blog creation", error: e.message });
+  }
+});
+
