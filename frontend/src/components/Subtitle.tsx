769525dd2eff79e1a42b9ae1ca4f624c3f0f4aee
Harshit Rai <raiharshit66@gmail.com>
2025-03-04 23:19:55 +0530

    Create a simple Subtitle UI component

--- frontend/src/components/Subtitle.tsx
+++ frontend/src/components/Subtitle.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-export const Subtitle = ({content , src,linkText})=>{
-    const  [text, setText] = useState("");
-    useEffect(()=>{
+export const Subtitle = ({ content, src, linkText } : {content : string , src : string , linkText  : string}) => {
+    const [text, setText] = useState("");
+    useEffect(() => {
         setText(content)
-    } ,[])
+    }, [])
     return <>
-    <div className="text-center text-slate-400 flex flex-rows-2"><div>{text}</div>
-    <Link className="ml-1 underline hover:text-black underline-offset-2"to={`${src}`}>{linkText}</Link></div>
+        <div className="text-center text-slate-400 flex flex-rows-2"><div>{text}</div>
+            <Link className="ml-1 underline hover:text-black underline-offset-2" to={`${src}`}>{linkText}</Link></div>
     </>
 
 }
