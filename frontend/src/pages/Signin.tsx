e9f554bb8975807132cda2aff4ca847cdf137454
Harshit Rai <raiharshit66@gmail.com>
2025-03-04 23:47:17 +0530

    Create the Signin page now

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,9 +3,9 @@ import { Quote } from "../components/Quote"
 export const Signin = () => {
     return <>
         <div className="w-screen grid grid-cols-1 md:grid-cols-2">
-                   <div className="">
-                       <Auth type="signin" />
-                   </div>
-                   <div className="invisible  md:visible"> <Quote msg="No  matter how much you try sometimes you can not change the results." author="Harshit Rai" design="CEO Apple"></Quote></div>
-               </div></>
+            <div className="">
+                <Auth type="signin" />
+            </div>
+            <div className="invisible  md:visible"> <Quote msg="No  matter how much you try sometimes you can not change the results." author="Harshit Rai" design="CEO Apple"></Quote></div>
+        </div></>
 }
