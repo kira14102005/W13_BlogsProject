77c363b28f3684fca2dead755d9da6fbf2426bf3
Harshit Rai <raiharshit66@gmail.com>
2025-03-04 22:14:33 +0530

    Create the basic hono app boilerplate and set routers accordingly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,4 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { verify, sign, decode } from 'hono/jwt'
-import { blogRouter } from '../routes/blogRouter'
-import { userRouter } from '../routes/userRouter'
 import { cors } from 'hono/cors'
 
 
@@ -15,22 +10,6 @@ const app = new Hono<{
   }
 }>()
 app.use('/*', cors())
-// app.use('/api/v1/blog/*' , async (c,next)=>{
-//   const body =  await c.req.json();
-//   const auth = await c.req.header("Authorization") || " "
-// const token  =auth?.split(' ')[1];
-//   const res = await verify(token , c.env.JWT_KEY)
-// if(res.id){
-//   await next()
-// }
-// else{
-//   // c.status(403)
-//   return c.json({
-//     msg : 'WRONG JWT ',
-//   token : token})
-// }
-// })
- //GPT SOLUTION FOR DIRECTLY CAUSING THE INTERNAL ERROR FAILURE  WE DO NEED TOUSE THE TRYCATCH  ALONG WITH
 
 app.route('/api/v1/blog/', blogRouter)
 app.route('/api/v1/user/', userRouter)
