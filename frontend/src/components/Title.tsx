905de5eff8e7fa59acc08e4b21ed0e4564d09a41
Harshit Rai <raiharshit66@gmail.com>
2025-03-04 23:22:33 +0530

    Similar Title component is created

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
-export const Title = ({content })=>{
-    const  [text, setText] = useState("");
-    useEffect(()=>{
+export const Title = ({ content }: { content: string }) => {
+    const [text, setText] = useState("");
+    useEffect(() => {
         setText(content)
-    } ,[])
+    }, [])
     return <>
-    <div className="text-center font-bold text-3xl">{text}</div>
+        <div className="text-center font-bold text-3xl">{text}</div>
     </>
 }
