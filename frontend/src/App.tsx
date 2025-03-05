2dc5a7761b7c21b0c106b29d622dd3c1758311a7
Harshit Rai <raiharshit66@gmail.com>
2025-03-06 01:30:05 +0530

    Add the client route to display the individual blog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
 import { Blog } from './pages/Blog'
 import { CreateBlog } from "./pages/CreateBlog"
+import { BlogDetails } from "./pages/IndividualBlog"
 function App() {
   // const [count, setCount] = useState(0)
 
@@ -15,7 +16,7 @@ function App() {
           <Route path='/signup' element={<Signup />}></Route>
           <Route path='/blogs' element={<Blog />}></Route>
           <Route path='/createBlog' element={<CreateBlog />}></Route>
-          <Route path='/blog/:id' element={<Blog />}></Route>
+          <Route path='/blog/:id' element={<BlogDetails />}></Route>
         </Routes>
       </BrowserRouter></>
   )
