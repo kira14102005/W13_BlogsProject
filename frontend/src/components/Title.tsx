d3fdc06ca9c38695b9e54ce00be58b612e445e8b
Harshit Rai <raiharshit66@gmail.com>
2025-03-05 23:08:44 +0530

    Correct the components using the unneccessary states

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const Title = ({ content }: { content: string }) => {
-    const [text, setText] = useState("");
-    useEffect(() => {
-        setText(content)
-    }, [])
     return <>
-        <div className="text-center font-bold text-3xl">{text}</div>
+        <div className="text-center font-bold text-3xl">{content}</div>
     </>
 }
