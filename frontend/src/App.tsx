12a714fd85b618c4e0f42d1d879c391fac6c6dd3
Harshit Rai <raiharshit66@gmail.com>
2025-03-05 23:49:48 +0530

    Include the route in the BroserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
 import { Blog } from './pages/Blog'
+import { CreateBlog } from "./pages/CreateBlog"
 function App() {
   // const [count, setCount] = useState(0)
 
@@ -13,6 +14,7 @@ function App() {
           <Route path='/signin' element={<Signin />}></Route>
           <Route path='/signup' element={<Signup />}></Route>
           <Route path='/blogs' element={<Blog />}></Route>
+          <Route path='/createBlog' element={<CreateBlog />}></Route>
           <Route path='/blog/:id' element={<Blog />}></Route>
         </Routes>
       </BrowserRouter></>
