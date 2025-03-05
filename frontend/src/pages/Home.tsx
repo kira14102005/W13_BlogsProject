45e02706d06c262d384a1a18220b61828cc5a64b
Harshit Rai <raiharshit66@gmail.com>
2025-03-06 03:10:37 +0530

    Create a logout function to handle clearing of cookie

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,16 +3,21 @@ import axios from "axios";
 import { BACKEND_URL } from "../config";
 import { Title } from "../components/Title";
 import { Sidebar } from "../components/SideBar";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
     const [userName, setUserName] = useState<string | null>(null);
+    const navigate = useNavigate();
 
     const getTokenFromCookies = useMemo(() => {
         const cookies = document.cookie.split("; ");
         const tokenCookie = cookies.find((cookie) => cookie.startsWith("token="));
         return tokenCookie ? tokenCookie.split("=")[1] : null;
     }, []);
-
+    const onLogout = () => {
+        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        window.location.href = "/";
+    }
     useEffect(() => {
         const fetchUser = async () => {
             try {
@@ -31,10 +36,12 @@ export const Home = () => {
 
     return (
         <div className="h-screen flex">
-            <Sidebar loggedIn= {userName!==null && userName!==undefined} />
+            <Sidebar loggedIn={userName !== null && userName !== undefined} onLogout={onLogout}
+            />
             <div className="flex-1 flex flex-col items-center p-5">
                 <Title content={`Welcome, ${userName || "Guest"}!`} />
                 <p className="mt-4 text-gray-700">Navigate using the sidebar.</p>
+
             </div>
         </div>
     );
