f2a033c8be6c5e673f1bc078a0fae6b716dcd2e5
Harshit Rai <raiharshit66@gmail.com>
2025-03-06 23:14:52 +0530

    Do the same for the validation middleware

--- backend/src/middlewares/validation.ts
+++ backend/src/middlewares/validation.ts
@@ -1,50 +1,21 @@
 import { createBlogInput, signedInInput, signedInput, updateBlogInput } from "@rrai21/iden34";
 import { Context, Next } from "hono";
 import { STATUS_CODES } from "../lib/constant";
-export const validateSignUp = async (c: Context, next: Next) => {
-  const body = await c.req.json();
-  const { success } = signedInput.safeParse(body);
 
-  if (!success) {
+const validateInput = async (c: Context, next: Next, schema: any) => {
+  const body = await c.req.json();
+  const parsed = schema.safeParse(body);
+console.log(body)
+  if (!parsed.success) {
     c.status(STATUS_CODES.BAD_REQUEST);
     return c.json({ msg: "Invalid Input", inform: "Failed Zod Validation" });
   }
-  await next(); 
+
+  c.set("body", body);
+  await next();
 };
-export const validateSignIn = async (c: Context, next: Next) => {
-    const body = await c.req.json();
-    const { success } = signedInInput.safeParse(body);
-  
-    if (!success) {
-      c.status(STATUS_CODES.BAD_REQUEST);
-      return c.json({ msg: "Invalid Input", inform: "Failed Zod Validation" });
-    }
-    await next(); 
-  };
-  
-  export const validateCreateBlog = async (c: Context, next: Next) => {
-    const body = await c.req.json();
-    const { success } = createBlogInput.safeParse(body);
-  
-    if (!success) {
-      c.status(STATUS_CODES.BAD_REQUEST);
-      return c.json({ msg: "Invalid Input", inform: "Failed Zod Validation" });
-    }
-  
-    c.set("body", body);
-    await next();
-  };
-  
-  export const validateUpdateBlog = async (c: Context, next: Next) => {
-    const body = await c.req.json();
-    const { success } = updateBlogInput.safeParse(body);
-  
-    if (!success) {
-      c.status(STATUS_CODES.BAD_REQUEST);
-      return c.json({ msg: "Invalid Input", inform: "Failed Zod Validation" });
-    }
-  
-    c.set("body", body);
-    await next();
-  };
-  
+
+export const validateSignUp = (c: Context, next: Next) => validateInput(c, next, signedInput);
+export const validateSignIn = (c: Context, next: Next) => validateInput(c, next, signedInInput);
+export const validateCreateBlog = (c: Context, next: Next) => validateInput(c, next, createBlogInput);
+export const validateUpdateBlog = (c: Context, next: Next) => validateInput(c, next, updateBlogInput);
