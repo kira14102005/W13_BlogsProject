eb0b6dc2c8e5bc77ea409d6ac0059d955e6006e8
Harshit Rai <raiharshit66@gmail.com>
2025-03-06 20:21:24 +0530

    Add a Custom button ui  to be used in other pages

---
+++ frontend/src/components/CustomButton.tsx
@@ -0,0 +1,8 @@
+export const CustomButton = ({ onclick, label }: { onclick: (e:any) => any, label: string }) => {
+    return <><button
+        onClick={onclick}
+        className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white  transition duration-200"
+    >
+        {label}
+    </button></>
+}
